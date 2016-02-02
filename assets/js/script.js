4f1e1bff7d6b26f407f6718b71cc3079ce44575f
Parvez Shaikh <parvez.shaikh@bookmyshow.com>
2016-02-02 15:22:19 +0530

    Different DOM ready event

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,22 @@
-$(document).ready(function(){
-	alert("DOM READY")
+/*---- Jquery Version -----*/
+// $(document).ready(function(){
+// 	alert("DOM READY")
+// })
+
+
+/*---- Shorter Jquery Version ----*/
+// $(function(){
+// 	alert("NEW DOM READY")
+// })
+
+/*---- Without Jquery ----*/
+// document.addEventListener('DOMContentLoaded', function(){
+// 	alert("WITHOUT JQUERY")
+// }, false);
+
+/*---- Works AnyWhere----*/
+r(function(){
+	alert("DOM READY!!!!!!")
 })
+
+function r(f) {/in/.test(document.readyState)?setTimeout('r('+f+')',9):f()}
